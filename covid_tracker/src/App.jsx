a830052c7f1d3d7c4ca9e7172af20bb5ff1bf8d0
shehan82 <shehansandeepa82@gmail.com>
2020-11-06 08:42:34 +0530

    create header and add css, and here i was use BEM css

--- covid_tracker/src/App.jsx
+++ covid_tracker/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css';
-import { Button, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import { Button, FormControl, InputLabel, Input, FormHelperText, MenuItem , Select} from '@material-ui/core';
+import { useState } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState([
+      "Sri lanka",
+      "USA",
+      "India",
+      "Japan",
+      "China"
+  ])
+
   return (
     <div className="app">
-      {/* header */}
-      <h1>Covid 19 tracker</h1>
-
+      <div className="app__header">
+        <h1>Covid 19 tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select>
+            
+          </Select>
+        </FormControl>
+    </div>
      
        
       {/* dropdown */}
