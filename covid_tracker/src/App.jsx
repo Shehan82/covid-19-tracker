b0acb7295501f40f3e714ef40c896f62b5fb4627
shehan82 <shehansandeepa82@gmail.com>
2020-11-06 07:15:31 +0530

    meterail UI added

---
+++ covid_tracker/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import { Button, FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="app">
+      {/* header */}
+      <h1>Covid 19 tracker</h1>
+
+     
+       
+      {/* dropdown */}
+
+      {/* small info boxes */}
+      {/* small info boxes */}
+      {/* small info boxes */}
+
+      {/* Large info boxes */}
+
+      {/* table */}
+
+      {/* graph */}
+
+
+      
+    
+      
+    </div>
+  );
+}
+
+export default App;
